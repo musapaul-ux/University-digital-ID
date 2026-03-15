9e8639caaf7eae189d7c626dc86f914c4c38a536
musapaul-ux <musapaul915@gmail.com>
2026-03-15 14:40:50 +0300

    modified the design layout

--- app/src/main/java/ug/ac/ndejje/studentid/MainActivity.kt
+++ app/src/main/java/ug/ac/ndejje/studentid/MainActivity.kt
@@ -3,7 +3,6 @@ package ug.ac.ndejje.studentid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,13 +21,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -43,13 +37,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import ug.ac.ndejje.studentid.ui.theme.StudentIDTheme
-import java.nio.file.Files.size
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,7 +63,7 @@ fun UniversityDigitalIDCard() {
     ElevatedCard(
         modifier = Modifier
             .width(360.dp)
-            .height(250.dp),
+            .height(230.dp),
         shape = RoundedCornerShape(16.dp),
         elevation = CardDefaults.elevatedCardElevation(8.dp)
     ) {
@@ -84,22 +74,34 @@ fun UniversityDigitalIDCard() {
             Row(
                 modifier = Modifier
                     .fillMaxSize()
-                    .alpha(0.07f),
-                horizontalArrangement = Arrangement.SpaceEvenly,
+                    .alpha(0.09f),
+                horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
+                Box(
+                    modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.CenterStart
+                ){
+                    Image(
+                        painter = painterResource(id = R.drawable.ndu_logo),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(140.dp)
+                            .padding(0.dp)
+                    )
+                }
 
-                Image(
-                    painter = painterResource(id = R.drawable.ndu_logo),
-                    contentDescription = null,
-                    modifier = Modifier.size(140.dp)
-                )
-
-                Image(
-                    painter = painterResource(id = R.drawable.ndu_logo),
-                    contentDescription = null,
-                    modifier = Modifier.size(140.dp)
-                )
+                Box(
+                    modifier = Modifier
+                        .weight(1f),
+                    contentAlignment = Alignment.CenterEnd
+                ){
+                    Image(
+                        painter = painterResource(id = R.drawable.ndu_logo),
+                        contentDescription = null,
+                        modifier = Modifier.size(140.dp)
+                    )
+                }
             }
 
             Column(
@@ -112,67 +114,76 @@ fun UniversityDigitalIDCard() {
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(70.dp)
+                        .height(60.dp)
                         .background(Color(0xFF8B1E1E))
                 )
 
-                Spacer(modifier = Modifier.height(45.dp))
+                Spacer(modifier = Modifier.height(35.dp))
 
                 // STUDENT NAME
-                Text(
-                    text = "VIOLA JOAN",
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.Bold
-                )
-
-                // PROGRAMME
-                Text(
-                    text = "Programme: BSc Computer Science",
-                    fontSize = 13.sp
-                )
-
-                // REGISTRATION NUMBER
-                Row {
-                    Text(
-                        text = "Registration Number: ",
-                        fontSize = 13.sp
-                    )
-                    Text(
-                        text = "24/U/1234",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 13.sp
-                    )
-                }
-
-                // DATES
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 10.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
+                    // STUDENT NAME
+                    Text(
+                        text = stringResource(R.string.student_name),
+                        style = MaterialTheme.typography.headlineSmall,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Spacer(modifier = Modifier.height(0.dp))
+
+                    // PROGRAMME
+                    Text(
+                        text = "Programme: BSc Computer Science",
+                        fontSize = 13.sp
+                    )
+                    Spacer(modifier = Modifier.height(0.dp))
+                    // REGISTRATION NUMBER
                     Row {
                         Text(
-                            text = "Date of Issue: ",
-                            fontSize = 12.sp
+                            text = "Registration Number: ",
+                            fontSize = 13.sp
                         )
                         Text(
-                            text = "01/02/2026",
+                            text = stringResource(R.string.registration_number),
                             fontWeight = FontWeight.Bold,
-                            fontSize = 12.sp
+                            fontSize = 13.sp
                         )
                     }
 
-                    Row {
-                        Text(
-                            text = "Expiry Date: ",
-                            fontSize = 12.sp
-                        )
-                        Text(
-                            text = "01/02/2029",
-                            fontSize = 12.sp
-                        )
+
+                    // DATES
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+
+                        Row {
+                            Text(
+                                text = "Date of Issue: ",
+                                fontSize = 12.sp
+                            )
+                            Text(
+                                text = stringResource(R.string.date_of_issue),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 12.sp
+                            )
+                        }
+
+                        Row {
+                            Text(
+                                text = "Expiry Date: ",
+                                fontSize = 12.sp
+                            )
+                            Text(
+                                text = stringResource(R.string.expiry_date),
+                                fontSize = 12.sp
+                            )
+                        }
                     }
                 }
 
@@ -193,14 +204,14 @@ fun UniversityDigitalIDCard() {
                     painter = painterResource(id = R.drawable.ndu_logo),
                     contentDescription = "Logo",
                     modifier = Modifier
-                        .size(85.dp)
+                        .size(75.dp)
                         .clip(CircleShape)
                 )
 
                 Image(
                     painter = painterResource(id = R.drawable.ug_flag),
                     contentDescription = "Flag",
-                    modifier = Modifier.size(65.dp)
+                    modifier = Modifier.size(60.dp)
                 )
             }
 
@@ -216,7 +227,7 @@ fun UniversityDigitalIDCard() {
                     painter = painterResource(id = R.drawable.student_photo),
                     contentDescription = "Student Photo",
                     modifier = Modifier
-                        .size(100.dp)
+                        .size(85.dp)
                         .clip(CircleShape)
                         .border(4.dp, Color(0xFF8B1E1E), CircleShape),
                     contentScale = ContentScale.Crop
